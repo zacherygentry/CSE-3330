b5cde6f2cfa68464dbd7ec485067b0dbae7f725d
Zachery Gentry <zacherygentry@gmail.com>
2018-10-01 18:20:45 -0500

    finished project 1

--- zcg4385_P1/zcg4385_c.sql
+++ zcg4385_P1/zcg4385_c.sql
@@ -4,18 +4,20 @@
 -- Student ID: 1001144385
 -- Assignment: Project #1
 
-
-DROP TABLE IF EXISTS Plane;
-CREATE TABLE Plane (
-ID int NOT NULL DEFAULT 1,
-Maker varchar(15) DEFAULT NULL,
-Model varchar(15) DEFAULT NULL,
-LastMaint DATE,
-LastMaintA varchar(15),
-PRIMARY KEY (ID)
+CREATE TABLE Airport (
+Code char(3),
+City varchar(15),
+State char(2),
+PRIMARY KEY (Code)
 );
 
-DROP TABLE IF EXISTS PlaneType;
+CREATE TABLE Flight (
+FLNO int,
+Meal varchar(15),
+Smoking char(1),
+PRIMARY KEY (FLNO)
+);
+
 CREATE TABLE PlaneType (
 Maker varchar(15) DEFAULT '',
 Model varchar(15) DEFAULT '',
@@ -24,86 +26,85 @@ GroundSpeed DECIMAL(10, 2),
 PRIMARY KEY (Maker, Model)
 );
 
-DROP TABLE IF EXISTS FlightLeg;
-CREATE TABLE FlightLeg (
-FLNO int,
-Seq varchar(15),
-FromA varchar(15),
-ToA varchar(15),
-DeptTime time,
-ArrTime time,
-Plane int,
-PRIMARY KEY (FLNO, Seq)
-);
-
-DROP TABLE IF EXISTS PlaneSeats;
-CREATE TABLE PlaneSeats (
-Maker varchar(15),
-Model varchar(15),
-SeatType varchar(15),
-NoOfSeats int,
-PRIMARY KEY (Maker, Model, SeatType)
-);
-
-DROP TABLE IF EXISTS Airport;
-CREATE TABLE Airport (
-Code char(3),
-City varchar(15),
-State char(2),
-PRIMARY KEY (Code)
-);
-
-DROP TABLE IF EXISTS FlightLegInstance;
-CREATE TABLE FlightLegInstance (
-Seq varchar(15),
-FLNO int,
-FDate DATE,
-ActDept varchar(15),
-ActArr varchar(15),
-Pilot int,
-PRIMARY KEY (Seq, FLNO, FDate)
-);
-
-DROP TABLE IF EXISTS Flight;
-CREATE TABLE Flight (
-FLNO int,
-Meal varchar(15),
-Smoking char(1),
-PRIMARY KEY (FLNO)
-);
-
-DROP TABLE IF EXISTS Pilot;
 CREATE TABLE Pilot (
 ID int,
 Name varchar(15),
-DateHired DATE,
+DateHired varchar(15),
 PRIMARY KEY (ID)
 );
 
-DROP TABLE IF EXISTS FlightInstance;
-CREATE TABLE FlightInstance (
-FLNO int,
-FDate DATE,
-PRIMARY KEY (FLNO)
-);
-
-DROP TABLE IF EXISTS Reservation;
-CREATE TABLE Reservation (
-PassID int,
-FLNO int,
-FDate DATE,
-FromA varchar(15),
-ToA varchar(15),
-SeatClass varchar(15),
-DateBooked DATE,
-DateCancelled DATE,
-PRIMARY KEY (PassID, FLNO, FDate)
-);
-
-DROP TABLE IF EXISTS Passenger;
 CREATE TABLE Passenger (
 ID int,
 Name varchar(15),
 Phone varchar(15),
 PRIMARY KEY (ID)
+);
+
+CREATE TABLE FlightInstance (
+FLNO int,
+FDate varchar(15),
+PRIMARY KEY (FLNO, FDate),
+FOREIGN KEY (FLNO) references Flight(FLNO)
+);
+
+CREATE TABLE PlaneSeats (
+Maker varchar(15),
+Model varchar(15),
+SeatType varchar(15),
+NoOfSeats int,
+PRIMARY KEY (Maker, Model, SeatType),
+FOREIGN KEY (Maker, Model) references PlaneType(Maker, Model)
+);
+
+CREATE TABLE Plane (
+ID int NOT NULL DEFAULT 1,
+Maker varchar(15) DEFAULT NULL,
+Model varchar(15) DEFAULT NULL,
+LastMaint varchar(15),
+LastMaintA varchar(15),
+PRIMARY KEY (ID),
+FOREIGN KEY (Maker, Model) references PlaneType(Maker, Model),
+FOREIGN KEY (LastMaintA) references Airport(Code)
+);
+
+CREATE TABLE FlightLeg (
+FLNO int,
+Seq varchar(15),
+FromA varchar(15),
+ToA varchar(15),
+DeptTime varchar(15),
+ArrTime varchar(15),
+Plane varchar(15),
+PRIMARY KEY (FLNO, Seq),
+FOREIGN KEY (FLNO) references Flight(FLNO),
+FOREIGN KEY (FromA) references Airport(Code),
+FOREIGN KEY (ToA) references Airport(Code),
+FOREIGN KEY (Plane) references Plane(ID)
+);
+
+CREATE TABLE FlightLegInstance (
+Seq varchar(15),
+FLNO int,
+FDate varchar(15),
+ActDept varchar(15),
+ActArr varchar(15),
+Pilot varchar(15),
+PRIMARY KEY (Seq, FLNO, FDate),
+FOREIGN KEY (Seq, FLNO) references FlightLeg(Seq, FLNO),
+FOREIGN KEY (FLNO, FDate) references FlightInstance(FLNO, FDate)
+);
+
+CREATE TABLE Reservation (
+PassID int,
+FLNO int,
+FDate varchar(15),
+FromA varchar(15),
+ToA varchar(15),
+SeatClass varchar(15),
+DateBooked varchar(15),
+DateCancelled varchar(15),
+PRIMARY KEY (PassID, FLNO, FDate),
+FOREIGN KEY (FLNO, FDate) references FlightInstance(FLNO, FDate),
+FOREIGN KEY (FromA) references Airport(Code),
+FOREIGN KEY (ToA) references Airport(Code)
 );
---
+++ zcg4385_P1/zcg4385_d.sql
@@ -0,0 +1,17 @@
+-- Class: CSE 3330
+-- Semester: Fall
+-- Student Name: Gentry, Zachery, zcg4385
+-- Student ID: 1001144385
+-- Assignment: Project #1
+
+DROP TABLE IF EXISTS Airport;
+DROP TABLE IF EXISTS Flight;
+DROP TABLE IF EXISTS PlaneType;
+DROP TABLE IF EXISTS Pilot;
+DROP TABLE IF EXISTS Passenger;
+DROP TABLE IF EXISTS FlightInstance;
+DROP TABLE IF EXISTS PlaneSeats;
+DROP TABLE IF EXISTS Plane;
+DROP TABLE IF EXISTS FlightLeg;
+DROP TABLE IF EXISTS FlightLegInstance;
+DROP TABLE IF EXISTS Reservation;
